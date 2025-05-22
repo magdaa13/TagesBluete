86831a3f98052b0ff52440847d2a5a9a739b6900
magdaa13 <magdalena.brabec@gmx.net>
2025-05-22 16:06:02 +0200

    Logos + Themes/Styles

--- app/src/main/java/at/fhj/tagesbluete/StartUebersicht.java
+++ app/src/main/java/at/fhj/tagesbluete/StartUebersicht.java
@@ -1,12 +1,17 @@
 package at.fhj.tagesbluete;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class StartUebersicht extends AppCompatActivity {
 
@@ -15,10 +20,11 @@ public class StartUebersicht extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start_uebersicht);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView dateTextview = findViewById(R.id.dateText);
+
+        String currentDate = new SimpleDateFormat("EEEE, dd. MMMM yyyy", Locale.GERMAN).format(new Date());
+
+        dateTextview.setText(currentDate);
     }
 }
